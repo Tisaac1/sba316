4ebab0e4b36cf7c5929bc6e89ee0cc80f654368a
Tisaac1 <lashay_isaac@yahoo.com>
2024-07-16 11:53:53 -0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function (){
   const mainTitle = document.getElementById("main-title")
 
   const List = document.getElementById("focus-list");
@@ -9,9 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const newItemButton = document.getElementById("new-item-button");
 
   //to change parent element style
+
   function UpdateListItems(e) {
     if (
-      document.parentElement.parentElement.querySelector("div").style.textDecoration ===
+      document.parentElement.querySelector("div").style.textDecoration ===
       ""
     ) {
       divElement.style.textDecoration = "line-through";
@@ -51,22 +52,21 @@ document.addEventListener("DOMContentLoaded", function () {
           document.parentElement.querySelector("div").innerText.trim() == element.item
         ) {
           element.status = true;
-        }
+        };
       });
 
     }
 
-
-
-
-
-
-    ;
     //Purpose: This line adds another event listener to the mainTitle element.
     //
     mainTitle.addEventListener("mouseout", function () {
 
       mainTitle.style.color = "white";
-    })
+    });
 
+const listItems = focusList.querySelectorAll("div")
+listItems.forEach((item) => {
+  item.addEventListener("click", updateListItems);
+  item.addEventListener("blur", handleListenItems)
+});
 
